ba0573a8eb91bed0a9f40f505995360602a5dbcf
Tembeka <thembeka.zulu96@gmail.com>
2023-07-23 06:01:04 -0700

    header file

--- hell.h
+++ hell.h
@@ -1,6 +1,15 @@
 #define HELL_H
 #ifdef HELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+
+int comm_execution(char* command);
 int main(void);
 int execve(const char *pathname, char *const argv[], char *const envp[]);
 
