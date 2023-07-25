aa14fca78b6e56427116863cc56e7eea884e1bbc
Tembeka <thembeka.zulu96@gmail.com>
2023-07-25 15:22:51 -0700

    header file

--- hell.h
+++ hell.h
@@ -8,7 +8,12 @@
 #include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 1024
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 10
 
+char* remov(char* str);
+int file(const char* dir, const char* file);
+int handle_exit(char* comm);
 int comm_execution(char* command);
 int main(void);
 int execve(const char *pathname, char *const argv[], char *const envp[]);
